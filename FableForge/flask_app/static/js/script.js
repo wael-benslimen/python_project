958ab55dbca576b2841b922d5819752159f01479
Raed-Galai <raedg6601@gmail.com>
2025-01-27 14:50:52 +0100

    templates and static

---
+++ FableForge/flask_app/static/js/script.js
@@ -0,0 +1,16 @@
+const eyes = document.querySelectorAll(".fa-solid")
+const password_inputs = document.querySelectorAll("input[type='password']")
+
+eyes.forEach((eye)=>{
+    eye.onclick = function() {
+        for(var i = 0; i<password_inputs.length; i++){
+            if (password_inputs[i].type == "text") {
+                password_inputs[i].type = "password"
+                eye.classList.replace("fa-eye","fa-eye-slash")
+            } else {
+                password_inputs[i].type = "text"
+                eye.classList.replace("fa-eye-slash","fa-eye")
+            }
+        }
+    }
+})
