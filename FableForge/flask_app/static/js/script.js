5e7289230fae1aaf23dfab639fd4995532ab558b
MortadhaHouch <97769483+MortadhaHouch@users.noreply.github.com>
2025-01-28 14:32:09 +0100

    next commit

--- FableForge/flask_app/static/js/script.js
+++ FableForge/flask_app/static/js/script.js
@@ -1,6 +1,7 @@
 const eyes = document.querySelectorAll(".fa-solid")
 const password_inputs = document.querySelectorAll("input[type='password']")
-
+const section = document.querySelector("section.login-register")
+const flipButtons = document.querySelectorAll("button.flip-1")
 eyes.forEach((eye)=>{
     eye.onclick = function() {
         for(var i = 0; i<password_inputs.length; i++){
@@ -12,5 +13,10 @@ eyes.forEach((eye)=>{
                 eye.classList.replace("fa-eye-slash","fa-eye")
             }
         }
+    }
+})
+flipButtons.forEach((button)=>{
+    button.onclick = function() {
+        section.classList.toggle("flip-2")
     }
 })
