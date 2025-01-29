a24204e4f5ed08ec383683f01c50ead88c2f201c
widedmsa <widedmsakem@gmail.com>
2025-01-29 10:26:30 +0100

    python

---
+++ FableForge/flask_app/static/edit_profile.js
@@ -0,0 +1,16 @@
+function click_photo() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.gear_img')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        }
+    }
+    }
+}
