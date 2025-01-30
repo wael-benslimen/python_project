470d264655aa424beb04c86226706f61d1fed533
wael-benslimen <waelbenslimen1111@gmail.com>
2025-01-30 13:14:50 +0100

    dashboard

--- FableForge/flask_app/static/js/dashboard_script.js
+++ FableForge/flask_app/static/js/dashboard_script.js
@@ -1,6 +1,8 @@
 const section1 = document.querySelector('.quest_create')
 const section2 = document.querySelector('.quest_view')
 const btn = document.querySelector('.plus_btn')
+const image = document.querySelector(".gear")
+const dropdown = document.querySelector(".dropdown_menu")
 
 function toggleClass() {
     section1.classList.toggle("hidden_face")
@@ -8,4 +10,7 @@ function toggleClass() {
     btn.classList.toggle("rotate_btn")
 }
 
+image.onclick = function () {
+    dropdown.classList.toggle("show")
+}
 
