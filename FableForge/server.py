ce810597131acc72d64cd5a58a23806111f3800e
wael-benslimen <waelbenslimen1111@gmail.com>
2025-02-04 18:54:14 +0100

    soket almost

--- FableForge/server.py
+++ FableForge/server.py
@@ -1,9 +1,8 @@
 from flask_app import app
 from flask_app.controllers import user_controller,landing_page_controller, dashboard,freinds_controller
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 from flask_cors import CORS
 from flask_app import socketio
-from flask_socketio import send
 from flask import session,request
 
 # Initialize SocketIO and CORS
@@ -15,17 +14,15 @@ def create_app():
     app.config['SECRET_KEY'] = 'secret!'
     return app
 
+@socketio.on('send-message')
+def handle_chat_message(msg):
+    print('Message: ' + msg["message"])
+    emit('receive-message', {"message":msg["message"],"username":msg['username']}, broadcast=True)
 
-@socketio.on('message')
-def handle_message(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
-@socketio.on("typing")
-def send(msg):
-    print(f"{msg['username']} is typing...")
-    send(f"{msg['username']} is typing...", room=msg['room'], broadcast=True)
-
-
+@socketio.on('typing')
+def handle_typing(data):
+    print(f"{data['username']} is typing...")
+    emit('typing', data, broadcast=True)
 
 if __name__ == "__main__":
     app = create_app()
